585d89980c27205c717f88445bdf1b1b7c1ebf1a
Emmett Underhill <eunderhi@redhat.com>
2016-02-26 16:59:39 -0500

    Initial abridged scraper implementation

--- util/scrape.py
+++ util/scrape.py
@@ -7,13 +7,17 @@ import requests
 
 class Scraper:
 
-    def __init__(self, destintation_file):
-        self.destintation_file = destintation_file
-        html = requests.get("http://www.foosball.com/learn/rules/ustsa/").text
-        self.soup = BeautifulSoup(html, 'lxml')
-
-    def parse(self):
+    def parse_long(self):
+        self.setup_soup("http://www.foosball.com/learn/rules/ustsa/")
         return self.__dfs(self.soup.ol)
+
+    def parse_short(self):
+        self.setup_soup("http://www.foosball.com/learn/rules/onepage/")
+        return self.short()
+
+    def setup_soup(self, url):
+        html = requests.get(url).text
+        self.soup = BeautifulSoup(html, 'lxml')
 
     def __dfs(self, node):
         if len(node.contents) <= 1:
@@ -21,6 +25,24 @@ class Scraper:
         else:
             return [self.__dfs(i) for i in node.findChildren()]
 
+    def short(self):
+        tree = {}
+        current_rule = ""
+        tables = self.soup.findAll("td")
+        table_list = tables[0].text.split("\n")
+        table_list.extend(tables[1].text.split("\n"))
+
+        for i in tables:
+            i = i.strip()
+            if self.startsWithNumber(i):
+                current_rule = i
+                tree[current_rule] = []
+            else:
+                tree[current_rule].append(i)
+        print tree
+
+    def startsWithNumber(self, i):
+        return i[0].isdigit()
+
 if __name__ == "__main__":
-    scrape = Scraper("test").parse()
-    print scrape
+    scrape = Scraper().parse_short()
